7298d1ae16c1bad9a704607dcb0da573f6b09dd0
J0J0 T <jojo@peek-a-boo.at>
2017-05-24 23:46:18 +0200

    debug out smoothing value for 8 knobs, send CC
      * still uses MIDI.h
      * should work with serial midi (DIN)

--- midictrl.ino
+++ midictrl.ino
@@ -10,8 +10,16 @@ static const unsigned PIN_LED_INT = 13;
 static const unsigned PIN_POT[] = {A0,A1,A2,A3,A4,A5,A6,A7}; // Uno max 6 inputs, Nano max 8
 uint8_t CC_Val[] = {0,0,0,0,0,0,0,0};
 uint8_t CC_ValOld[] = {0,0,0,0,0,0,0,0};
-uint8_t PotVal[] = {0,0,0,0,0,0,0,0};
-uint8_t i = 0;
+uint16_t PotVal[] = {0,0,0,0,0,0,0,0};
+uint16_t PotValOld[] = {0,0,0,0,0,0,0,0};
+uint8_t i = 8;
+// smoothing
+int PotSmoothVal[] = {0,0,0,0,0,0,0,0};
+int PotQuickSmoothVal[] = {0,0,0,0,0,0,0,0};
+int PotValDiff[] = {0,0,0,0,0,0,0,0};
+bool QuickMode[] = {false,false,false,false,false,false,false,false};
+//int x[] = {0,0,0,0,0,0,0,0};
+int x = 0;
 
 //MIDI_CREATE_DEFAULT_INSTANCE();
 MIDI_CREATE_INSTANCE(SoftwareSerial, MIDIserial, MIDI);
@@ -27,11 +35,50 @@ void setup() {
 void loop() {
   for (i=0; i<CC_COUNT; i++) {
     PotVal[i] = analogRead(PIN_POT[i]);
-    CC_Val[i] = map(PotVal[i],0,1023,0,127);
-    if (CC_Val[i] != CC_ValOld[i]) {
-      USBserial.print("PotVal: ");USBserial.println(PotVal[i]);
+    PotSmoothVal[i] = 0.6 * PotSmoothVal[i] + 0.4 * PotVal[i];
+    PotValDiff[i] = abs(PotVal[i] - PotValOld[i]);
+    if (PotValDiff[i] <= 5 && QuickMode[i] == false) {
+      PotQuickSmoothVal[i] = 0.6 * PotSmoothVal[i] + 0.4 * PotVal[i];
+    }
+    else if (PotValDiff[i] <= 5 && QuickMode[i] == true){
+      PotQuickSmoothVal[i] = PotVal[i];
+      x++;
+    }
+    else {
+      PotQuickSmoothVal[i] = PotVal[i];
+      QuickMode[i] = true;
+    }
+    if (x >= 10) {
+      x = 0;
+      QuickMode[i] = false;
+    }
+    CC_Val[i] = map(PotQuickSmoothVal[i],0,1023,0,127);
+    //if (CC_Val[i] != CC_ValOld[i]) {
+    if (PotVal[i] != PotValOld[i]) {
+     /* USBserial.print("PotSmoothVal: ");USBserial.println(PotSmoothVal[i]);
+      USBserial.print("PotQuickSmootVal: ");USBserial.println(PotQuickSmoothVal[i]);
+      USBserial.print("PotValDiff: ");USBserial.println(PotValDiff[i]);
+      USBserial.print("QuickMode: ");USBserial.println(QuickMode[i]);
+      USBserial.print("x: ");USBserial.println(x[i]);
       USBserial.print("CC_Val: ");USBserial.println(CC_Val[i]);
+    */
+      USBserial.print("\t");
+      USBserial.print(PotVal[i]);
+      USBserial.print("\t");
+      USBserial.print(PotSmoothVal[i]);
+      USBserial.print("\t");
+      USBserial.print(PotValDiff[i]);
+      USBserial.print("\t");
+      USBserial.print(PotQuickSmoothVal[i]);
+      USBserial.print("\t");
+      USBserial.print(QuickMode[i]);
+      USBserial.print("\t");
+      USBserial.print(x);
+      USBserial.print("\t");
+      USBserial.println(CC_Val[i]);
+      MIDI.sendControlChange(CC_NUM[i], CC_Val[i], MIDI_CH);
       CC_ValOld[i] = CC_Val[i];
+      PotValOld[i] = PotVal[i];
     }
   }
 
