88cb33ddf86c117e802c0d93002a6114fe4dd46e
J0J0 T <jojo@peek-a-boo.at>
2017-05-25 23:19:42 +0200

    common serial bitrate for hairless midi bridge
      * 9600 bps
      * simply write 3 bytes
      * works on non-usb arduino
      * needs serial-midi converter software like hairless

--- midictrl.ino
+++ midictrl.ino
@@ -1,9 +1,6 @@
-#include <MIDI.h>
-#include <SoftwareSerial.h>
 #define USBserial Serial
-SoftwareSerial MIDIserial(0, 1); // RX, TX
 
-static const unsigned MIDI_CH = 1; //configure MIDI channel here
+static const unsigned MIDI_CMD = 176; // 176 = CC on channel 1
 static const unsigned CC_COUNT = 8; // configure how many knobs here
 static const unsigned CC_NUM[] = {81,82,83,84,85,86,87,88}; // configure MIDI CC numbers here
 static const unsigned PIN_LED_INT = 13;
@@ -21,15 +18,10 @@ bool QuickMode[] = {false,false,false,false,false,false,false,false};
 //int x[] = {0,0,0,0,0,0,0,0};
 int x = 0;
 
-//MIDI_CREATE_DEFAULT_INSTANCE();
-MIDI_CREATE_INSTANCE(SoftwareSerial, MIDIserial, MIDI);
-
 void setup() {
   pinMode(PIN_LED_INT, OUTPUT);
-  MIDI.begin(MIDI_CH); // Launch MIDI and set to channel 1
   while(!USBserial); // wait until USBserial is accessible 
-  USBserial.begin(115200); // DEBUG here
-  USBserial.println("Arduino midictrl ready");
+  USBserial.begin(9600); // common serial rate -> hairless midi bridge
 }
 
 void loop() {
@@ -53,16 +45,9 @@ void loop() {
       QuickMode[i] = false;
     }
     CC_Val[i] = map(PotQuickSmoothVal[i],0,1023,0,127);
-    //if (CC_Val[i] != CC_ValOld[i]) {
-    if (PotVal[i] != PotValOld[i]) {
-     /* USBserial.print("PotSmoothVal: ");USBserial.println(PotSmoothVal[i]);
-      USBserial.print("PotQuickSmootVal: ");USBserial.println(PotQuickSmoothVal[i]);
-      USBserial.print("PotValDiff: ");USBserial.println(PotValDiff[i]);
-      USBserial.print("QuickMode: ");USBserial.println(QuickMode[i]);
-      USBserial.print("x: ");USBserial.println(x[i]);
-      USBserial.print("CC_Val: ");USBserial.println(CC_Val[i]);
-    */
-      USBserial.print("\t");
+    if (CC_Val[i] != CC_ValOld[i]) {
+    //if (PotVal[i] != PotValOld[i]) { // DEBUG START
+    /*USBserial.print("\t");
       USBserial.print(PotVal[i]);
       USBserial.print("\t");
       USBserial.print(PotSmoothVal[i]);
@@ -75,8 +60,11 @@ void loop() {
       USBserial.print("\t");
       USBserial.print(x);
       USBserial.print("\t");
-      USBserial.println(CC_Val[i]);
-      MIDI.sendControlChange(CC_NUM[i], CC_Val[i], MIDI_CH);
+      USBserial.println(CC_Val[i]); // DEBUG END
+     */
+      USBserial.write(MIDI_CMD);
+      USBserial.write(CC_NUM[i]);
+      USBserial.write(CC_Val[i]);
       CC_ValOld[i] = CC_Val[i];
       PotValOld[i] = PotVal[i];
     }
